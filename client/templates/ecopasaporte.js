eea59c424ff1c2466bebf7919d27888bd4438a1c
edu2969 <edtronco@gmail.com>
2021-06-09 20:08:22 -0400

    Menu settings 1era version
    - Versión inicial de panel de preferencias
    - Logout - Login

--- client/templates/ecopasaporte.js
+++ client/templates/ecopasaporte.js
@@ -1,10 +1,37 @@
 import { Nivel } from '../utils/nivel'
 import { ECOActividades } from '../../lib/ECOActividades' 
 
+const menuPrincipal = (template) => {
+	UIUtils.toggle("cruz-principal", "oculto");
+	UIUtils.toggle("tombola", "flotalatombola5x");
+	UIUtils.toggle("tombola", "flotalatombola2x");
+	UIUtils.toggle("tombola", "desaparece");
+	setTimeout(function() {
+		ECOActividades.set([{
+			espacio: true 
+		}, {
+			espacio: true
+		}, {
+			nombre: "identificate",
+			icono: "fingerprint",
+			activo: true,
+			accion: "Soy <b>&hearts;</b> verde"
+		}, {
+			nombre: "sabermas",
+			icono: "contact_support",
+			accion: "Quiero saber"
+		}]);
+		UIUtils.toggle("tombola", "reaparece");
+		UIUtils.toggle("tombola", "desaparece");
+		setTimeout(function() {
+			UIUtils.toggle("tombola", "reaparece");
+		}, 500);
+	}, 500);
+}
+
 Template.ecopasaporte.onCreated(function() {
 	ECOActividades.init();
 	this.panel = new ReactiveVar(false);
-	Nivel.setNivelUsuario();
 });
 
 Template.ecopasaporte.rendered = function() {		
@@ -26,9 +53,10 @@ Template.ecopasaporte.rendered = function() {
 		}, 950);
 		
 	}, 1500);
-	
-	Tracker.autorun(() => {
-	});
+
+	if(Meteor.userId()) {
+		$(".menu-preferencias").toggleClass("oculto");
+	}
 }
 
 Template.ecopasaporte.helpers({
@@ -61,7 +89,6 @@ Template.ecopasaporte.helpers({
 Template.ecopasaporte.events({
 	"click .actividad"(e, template) {
 		let actividad = e.currentTarget.classList.value;
-		
 		if(actividad.indexOf("sabermas")!=-1) {
 			UIUtils.toggle("tombola", "desaparece");
 			UIUtils.toggle("tombola", "flotalatombola2x");
@@ -147,145 +174,15 @@ Template.ecopasaporte.events({
 		}, 1000);
 	},
 	"click .cruz-principal"(e, template) {
-		UIUtils.toggle("cruz-principal", "oculto");
-		UIUtils.toggle("tombola", "flotalatombola5x");
-		UIUtils.toggle("tombola", "flotalatombola2x");
-		UIUtils.toggle("tombola", "desaparece");
-		setTimeout(function() {
-			ECOActividades.set([{
-				espacio: true 
-			}, {
-				espacio: true
-			}, {
-				nombre: "identificate",
-				icono: "fingerprint",
-				activo: true,
-				accion: "Soy <b>&hearts;</b> verde"
-			}, {
-				nombre: "sabermas",
-				icono: "contact_support",
-				accion: "Quiero saber"
-			}]);
-			UIUtils.toggle("tombola", "reaparece");
-			UIUtils.toggle("tombola", "desaparece");
-			setTimeout(function() {
-				UIUtils.toggle("tombola", "reaparece");
-			}, 500);
-		}, 500);
+		menuPrincipal(template);
+	},
+	"click .menu-preferencias"(e, template) {
+		$(".panel-preferencias").toggleClass("activo");
+	},
+	"click .opcion-logout"(e, template) {
+		$(".menu-preferencias").toggleClass("oculto");
+		$(".panel-preferencias").toggleClass("activo");
+		Meteor.logout();
+		menuPrincipal(template);
 	}
 });
-
-/*
-
-Template.entregas.onCreated(function() {
-	this.nivel = new ReactiveVar("7Ab");
-});
-
-Template.entregas.rendered = function() {	
-	const instance = Template.instance();
-	Tracker.autorun(() => {
-		Meteor.subscribe('curso', instance.nivel.get());
-		Meteor.subscribe('desafios');
-		Meteor.subscribe('entregasPorNivel', instance.nivel.get());
-	});
-}
-
-Template.entregas.helpers({
-	alumnos() {
-		const nivel = Template.instance().nivel.get();
-		return Meteor.users.find({ "profile.curso": nivel }).map(function(alumno) {
-			let entregas = Tareas.find().map(function(tarea) {
-				const entrega = Entregas.findOne({ tareaId: tarea._id, alumnoId: alumno._id });
-				let docEntrega = {
-					celda: EVALUACIONES[( entrega && entrega.evaluacion ) || ( entrega && "OK" ) || "SR"]			
-				};
-				if( entrega && entrega._id ) {
-					docEntrega._id = entrega._id;
-				}
-				return docEntrega;
-			});
-			return {
-				_id: alumno._id,
-				nombres: alumno.profile.nombres,
-				apellidos: alumno.profile.apellidos,
-				entregas: entregas
-			}
-		});
-	},
-	tareas() {
-		const nivel = Template.instance().nivel.get();
-		return Tareas.find().map(function(tarea) {
-			let entregas = Meteor.users.find({ "profile.curso": nivel })
-			return {
-				_id: tarea._id,
-				fecha: tarea.desde
-			}
-		})
-	}
-})
-
-Template.entregas.events({
-	"click th.rotated"(e) {
-		const renderTarea = (tarea) => {
-			$('#input-desde').datetimepicker({
-				format: 'DD/MM/YYYY HH:mm',
-				defaultDate: moment(tarea.desde, "DD/MM/YYYY HH:mm")
-			});
-
-			$('#input-hasta').datetimepicker({
-				format: 'DD/MM/YYYY HH:mm',
-				defaultDate: moment(tarea.hasta, "DD/MM/YYYY HH:mm")
-			});
-
-			let tipo = "video";
-			if( tarea.url ) 
-				tipo = "url"; 
-			else if( tarea.youtube ) 
-				tipo = "youtube";
-			Session.set("TareaSeleccionada", tarea);
-			$("#summernote").summernote("code", tarea.descripcion ? tarea.descripcion : "");
-			document.querySelector(".cuadro-capsula").style.display = 'none';
-			document.querySelector("#tipo-" + tipo).style.display = 'block';
-			document.querySelector("#radio-" + tipo).checked = true;
-			document.querySelector(".contenedor-tarea").classList.toggle("activo");
-		}
-		
-		const id = e.currentTarget.id;
-		["video", "youtube", "url"].forEach(function(tipo) {
-			document.querySelector("#tipo-" + tipo).style.display = 'none';
-		});
-		let tarea = {};
-		if( id ) {
-			tarea = Tareas.findOne({ _id: id });
-			renderTarea(tarea);
-		} else {
-			tarea.desde = moment().startOf("day").hour(8).toDate();
-			tarea.hasta = moment().startOf("day").add(44, "hour").toDate();
-			Session.set("TareaSeleccionada", tarea);
-			Meteor.call("GuardarTarea", false, tarea, function(err, resp) {
-				if(!err) {
-					tarea._id = resp;
-					renderTarea(tarea);
-				} else {
-					console.ward("Ha habido un error al crear la tarea");
-				}
-			});			
-		}
-	},
-	"click td"(e) {
-		const id = e.currentTarget.id;
-		if(!id) return;
-		const entrega = Entregas.findOne({ _id: id });
-		Session.set("Seleccion", {
-			tarea: Tareas.findOne({ _id: entrega.tareaId }),
-			entrega: entrega
-		});
-    document.querySelector(".contenedor-revision").classList.toggle("activo");
-	},
-	"change #curso"(e, template) {
-		const nivel = e.currentTarget.value;
-		template.nivel.set(nivel);
-	}
-});
-
-*/
