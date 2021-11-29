1fa240e50d996fb128235c7e6173ed153390feed
edu2969 <edtronco@gmail.com>
2021-11-29 13:41:30 -0500

    Acciones
    - Mejora visual

--- client/templates/acciones/acciones.js
+++ client/templates/acciones/acciones.js
@@ -1,9 +1,15 @@
-import { ECOAcciones, ECOOrganizaciones } from '../../../lib/collections/ECODimensionesCollections'
+import { ECOAcciones, ECOOrganizaciones } from '../../../lib/collections/ECODimensionesCollections';
+import { Images } from '../../../lib/collections/FilesCollections';
+import { LISTADO_MATERIALES } from '../../../lib/constantes';
+import { UIUtils, FormUtils, IsEmpty } from '../../utils/utils';
 
 Template.acciones.onCreated(function () {
   this.accionSeleccionada = new ReactiveVar(false);
+  this.enListado = new ReactiveVar(true);
+  this.editando = new ReactiveVar(false);
 
-  Meteor.subscribe('eco_organizaciones')
+  Meteor.subscribe('eco_organizaciones');
+  Meteor.subscribe('eco_organizaciones.participantes');
   Meteor.subscribe('eco_organizaciones.imagenes');
   this.autorun(() => {
     Meteor.subscribe('eco_acciones');
@@ -31,6 +37,7 @@ Template.acciones.helpers({
   resultado() {
     const cuadrillaId = Template.instance().accionSeleccionada.get();
     const query = cuadrillaId ? { cuadrillasId: cuadrillaId } : {};
+    query.tipo = "RM";
     return ECOAcciones.find(query).map((item, indice) => {
       const fr = moment(item.fechaRetiro);
       const dif = moment().diff(fr, item.periodicidad == "M" ? "month" : "week") + 1;
@@ -51,6 +58,48 @@ Template.acciones.helpers({
     const cuadrillaId = Template.instance().accionSeleccionada.get();
     const cuadrilla = ECOOrganizaciones.findOne({ _id: cuadrillaId });
     return cuadrilla.nombre;
+  },
+  accion() {
+    const data = Template.instance().accionSeleccionada.get();
+    if (!data) return false;
+    const accion = JSON.parse(JSON.stringify(data));
+    const elementos = accion.materiales?.split(",");
+    accion.materiales = elementos?.map((elemento) => {
+      return LISTADO_MATERIALES.find((elem) => {
+        return elem.id == elemento;
+      });
+    });
+    const fr = moment(accion.fechaRetiro);
+    const dif = moment().diff(fr, accion.periodicidad == "M" ? "month" : "week") + 1;
+    accion.proximoRetiro = moment(fr).add(dif, accion.periodicidad == "M" ? "month" : "week").format("DD MMM'YY");
+    const cuadrilla = ECOOrganizaciones.findOne({
+      _id: accion.cuadrillasId
+    });
+    const imgC = Images.findOne({
+      userId: cuadrilla.usuarioId,
+      "meta.tipo": "ecoorganizacion",
+    });
+    cuadrilla.avatar = imgC?.link();
+    const owner = Meteor.users.findOne({ _id: cuadrilla.usuarioId });
+    cuadrilla.direccion = owner.profile.direccion;
+    accion.cuadrilla = cuadrilla;
+
+    accion.integrantes = cuadrilla.integrantes?.map((integrante) => {
+      const img = Images.findOne({
+        userId: integrante.usuarioId,
+        meta: {},
+      });
+      const user = Meteor.users.findOne({ _id: integrante.usuarioId });
+      const nombre = user.profile.nombre;
+      const iniciales = nombre[0].charAt(0) + (nombre.length > 1 ? nombre[1].charAt(0) : "")
+      return {
+        nombre: nombre,
+        direccion: user.profile.direccion,
+        avatar: img ? img.link() : false,
+        iniciales: iniciales
+      }
+    })
+    return accion;
   }
 });
 
@@ -60,6 +109,22 @@ Template.acciones.events({
     template.accionSeleccionada.set(doc._id);
   },
   "click ul"(e, template) {
-    console.log(e.currentTarget.id);
-  }
+    const id = e.currentTarget.id;
+    if (!id) return;
+    const entidad = ECOAcciones.findOne({ _id: id });
+    template.enListado.set(false);
+    template.accionSeleccionada.set(entidad)
+    UIUtils.toggle("carrousel", "grilla");
+    UIUtils.toggle("carrousel", "detalle");
+    UIUtils.toggle("navegacion-atras", "activo");
+  },
+  "click .navegacion-atras"(e, template) {
+    UIUtils.toggle("carrousel", "grilla");
+    UIUtils.toggle("carrousel", "detalle");
+    UIUtils.toggle("navegacion-atras", "activo");
+    template.accionSeleccionada.set(false);
+    template.editando.set(false);
+    template.enListado.set(true);
+    $(".detalle").scrollTop(0);
+  },
 })
